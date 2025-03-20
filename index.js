cd769dcf3d85594f6351214fdc59df800394787a
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-03-20 12:04:15 +0300

    uses the array find method

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+
+
+function superbowlWin(record) {
+    let win = record.find(record => record.result === "W")
+    return win ? win.year : undefined
+}
+superbowlWin(record);
+// console.log(superbowlWin(record)) // 2015
